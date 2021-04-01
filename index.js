beb8f8b8effeca6b99c8690d41c0a8d1d31c8453
carlosb <carlos.benner.b@gmail.com>
2021-04-01 12:01:48 -0300

    Revert "Fixing calculation and display bugs."
    This reverts commit 3601fdbe8e6d3f830b56b818c78b4855424b4670.

--- index.js
+++ index.js
@@ -1,65 +1,22 @@
 /* Calculator logic */
 
-const enterKeyCode = 'Enter';
-const escapeKeyCode = 'Escape';
-const backspaceKeyCode = 'Backspace';
-
-const keyTypes = {
-    clear: 'clear',
-    clearOnce: 'clearOnce',
-    solve: 'solve',
-    digit: 'digit',
-    operation: 'operation',
-};
-
 //Input
 let currentInput = '';
-let lastKey = keyTypes.clear;
-
+let lastKey = 'clear';
+let operationsArray = ['+', '-', '*', '/', 'exp'];
 document.querySelectorAll('.calc-pad > button').forEach((btn) => {
     btn.addEventListener('click', buttonPressed);
 });
-
-document.addEventListener('keydown', keyboardPressed);
-
-function keyboardPressed(e) {
-    if (e.repeat) return;
-    let key = e.key;
-    if (e.key == '/') e.preventDefault;
-    switch (key) {
-        case enterKeyCode:
-            key = keyTypes.solve;
-            break;
-        case escapeKeyCode:
-            key = keyTypes.clear;
-            break;
-        case backspaceKeyCode:
-            key = keyTypes.clearOnce;
-            break;
-        default:
-            break;
-    }
-    const calcButton = document.querySelector(
-        `.calc-pad > button[data-key='${key}']`
-    );
-    if (calcButton) {
-        calcButton.click();
-    }
-}
 
 function buttonPressed(e) {
     const screen = document.querySelector('.calc-screen > .screen');
     const keyValue = e.target.attributes['data-key'].value;
     const keyType = e.target.attributes['data-key-type']?.value;
 
-    //Clearing input for special case
-    currentInput = currentInput === 'ERR' ? '0' : currentInput;
-
-    //Appending space to separate operators and digits
     if (
-        keyType != keyTypes.clear &&
-        keyType != keyTypes.clearOnce &&
-        keyType != keyTypes.solve &&
+        keyType != 'clear' &&
+        keyType != 'clearOnce' &&
+        keyType != 'solve' &&
         keyType != lastKey &&
         currentInput != '' &&
         !currentInput.endsWith(' ')
@@ -68,44 +25,34 @@ function buttonPressed(e) {
     }
 
     switch (keyType) {
-        case keyTypes.clear:
+        case 'clear':
             clearScreen();
             break;
-
-        case keyTypes.clearOnce:
+        case 'clearOnce':
             currentInput = currentInput.split('').slice(0, -1).join('');
-            //Clear screen if all input were removed or if character is not a digit
-            if (
-                currentInput.length == 0 ||
-                (currentInput.length == 1 && !Number.isInteger(+currentInput))
-            ) {
+            if (currentInput.length == 0) {
                 clearScreen();
             }
             break;
-
-        case keyTypes.solve:
-            //Ignore previous input if solving equation with trailing operation
-            if (lastKey == keyTypes.operation) {
+        case 'solve':
+            if (lastKey == 'operation') {
+                console.log('last key operation', currentInput);
                 currentInput = currentInput.split('').slice(0, -1).join('');
+                console.log(currentInput);
             }
             currentInput = solveEquation(currentInput);
             break;
-
-        case keyTypes.digit:
+        case 'grouping':
+            //TODO: check if can close brackets or add new bracket
+            break;
+        case 'digit':
             let inputArray = currentInput.split(' ');
-            if (lastKey === keyTypes.solve) currentInput = '';
+            if (lastKey === 'solve') currentInput = '';
             if (keyValue === '.') {
                 //ignore key if last number contains a dot
                 if (inputArray[inputArray.length - 1].indexOf('.') > -1) break;
-                //include leading zero if dot is used and there is no digit
-                if (
-                    !Number.isInteger(
-                        parseInt(currentInput[currentInput.length - 1])
-                    )
-                )
-                    currentInput += '0';
+                if (currentInput === '') currentInput = '0';
             }
-            //ignore zero input if leading zero exists and no decimal separator present
             if (
                 keyValue === '0' &&
                 inputArray[inputArray.length - 1].startsWith('0') &&
@@ -115,15 +62,13 @@ function buttonPressed(e) {
             }
             currentInput += `${keyValue}`;
             break;
-
-        case keyTypes.operation:
+        case 'operation':
             //replace last operation
-            if (lastKey === keyTypes.operation)
+            if (lastKey === 'operation')
                 currentInput = currentInput.split('').slice(0, -1).join('');
-            if (currentInput === '') currentInput = '0 ';
+            if (currentInput === '') currentInput = '0';
             currentInput += `${keyValue}`;
             break;
-
         default:
             break;
     }
@@ -132,10 +77,7 @@ function buttonPressed(e) {
         screen.textContent = currentInput;
         screen.classList.remove('blink');
     }
-    lastKey =
-        keyType != null && keyType != keyTypes.clearOnce
-            ? keyType
-            : keyTypes.digit;
+    lastKey = keyType != null && keyType != 'clearOnce' ? keyType : 'digit';
 }
 
 function clearScreen() {
@@ -146,7 +88,12 @@ function clearScreen() {
 }
 
 function solveEquation(eq) {
+    //TODO: division and multiplication must have same priority.
+    //TODO: addition and subtraction must have same priority.
+    //At this time because of the switch division and addition have higher priority. It should be from left to right.
+
     let equationArray = eq.split(' ');
+
     let exponentIndex = equationArray.indexOf('^');
     let divisionIndex = equationArray.indexOf('/');
     let multiplicationIndex = equationArray.indexOf('*');
@@ -155,51 +102,48 @@ function solveEquation(eq) {
 
     switch (true) {
         case exponentIndex > -1:
-            equationArray = solveOperationInArray(equationArray, exponentIndex);
+            equationArray = [
+                ...equationArray.slice(0, exponentIndex - 1),
+                equationArray[exponentIndex - 1] **
+                    equationArray[exponentIndex + 1],
+                ...equationArray.slice(exponentIndex + 2),
+            ];
             break;
-        case divisionIndex > -1 || multiplicationIndex > -1:
-            equationArray = solveOperationInArray(
-                equationArray,
-                getLowerOperationIndex(divisionIndex, multiplicationIndex)
-            );
+        case divisionIndex > -1:
+            equationArray = [
+                ...equationArray.slice(0, divisionIndex - 1),
+                equationArray[divisionIndex - 1] /
+                    equationArray[divisionIndex + 1],
+                ...equationArray.slice(divisionIndex + 2),
+            ];
             break;
-        case additionIndex > -1 || subtractionIndex > -1:
-            equationArray = solveOperationInArray(
-                equationArray,
-                getLowerOperationIndex(additionIndex, subtractionIndex)
-            );
+        case multiplicationIndex > -1:
+            equationArray = [
+                ...equationArray.slice(0, multiplicationIndex - 1),
+                equationArray[multiplicationIndex - 1] *
+                    equationArray[multiplicationIndex + 1],
+                ...equationArray.slice(multiplicationIndex + 2),
+            ];
+            break;
+        case additionIndex > -1:
+            equationArray = [
+                ...equationArray.slice(0, additionIndex - 1),
+                +equationArray[additionIndex - 1] +
+                    +equationArray[additionIndex + 1],
+                ...equationArray.slice(additionIndex + 2),
+            ];
+            break;
+        case subtractionIndex > -1:
+            equationArray = [
+                ...equationArray.slice(0, subtractionIndex - 1),
+                equationArray[subtractionIndex - 1] -
+                    equationArray[subtractionIndex + 1],
+                ...equationArray.slice(subtractionIndex + 2),
+            ];
             break;
         default:
-            //Exit condition: All operations were solved.
-            const num = equationArray.join(' ');
-            return (Math.round(num * 10000) / 10000).toString();
-
+            return equationArray.join(' ');
             break;
     }
-    //We solve the equation recursively by operations according to their "weight".
-    //First power, then multiplication and division from left to right,
-    //finally addition and subtraction from left to right
     return solveEquation(equationArray.join(' '));
-}
-
-function getLowerOperationIndex(op1, op2) {
-    return op1 === -1 ? op2 : op2 === -1 ? op1 : Math.min(op1, op2);
-}
-
-function solveOperationInArray(eqArray, operationIndex) {
-    if (operationIndex < 0) return eqArray;
-    if (eqArray[operationIndex] === '/' && eqArray[operationIndex + 1] == 0)
-        return ['ERR'];
-
-    //using eval here. Can be replaced by a switch handling each type of operation
-    const operationResult = eval(
-        `${eqArray[operationIndex - 1]} ${
-            eqArray[operationIndex] === '^' ? '**' : eqArray[operationIndex]
-        } ${eqArray[operationIndex + 1]}`
-    );
-    return [
-        ...eqArray.slice(0, operationIndex - 1),
-        operationResult,
-        ...eqArray.slice(operationIndex + 2),
-    ];
 }
